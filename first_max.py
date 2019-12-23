71a7827d565fa186f29e85f6f925549e1cb1c26c
Yao-Phoenix <493867456@qq.com>
2019-12-23 16:03:59 +0800

    first_max

---
+++ first_max.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+
+def max2(x):
+    m1, m2 = (x[0], x[1]) if x[0] > x[1] else (x[1], x[0])
+    for index in range(2, len(x)):
+        if x[index] > m1:
+            m2 = m1
+            m1 = x[index]
+        elif x[index] > m2:
+            m2 = x[index]
+    return m1, m2
+
+if __name__ == '__main__':
+    print(max2([1, 5, 3, 6]))
