61600c687f4634c4d663e0f8e9e94f2a15972b56
Yao-Phoenix <493867456@qq.com>
2020-01-02 15:59:08 +0800

    game_base

---
+++ game_test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import pygame
+
+def main():
+    # 初始化导入的 pygame 中的模块
+    pygame.init()
+    # 初始化用于显示的窗口并设置窗口尺寸
+    screen = pygame.display.set_mode((800, 600))
+    # 设置当前窗口的标题
+    pygame.display.set_caption('大球吃小球')
+    '''
+    # 设置窗口的背景色(颜色是有红绿蓝三原色组成的元组)
+    screen.fill((242, 242, 242))
+    # 绘制一个圆(参数分别是: 屏幕, 颜色, 圆心位置, 半径, 0 表示填充圆)
+    pygame.draw.circle(screen, (255, 0, 0), (100, 100), 30, 0)
+    # 刷新当前窗口(渲染窗口将绘制的图像呈现出来)
+    pygame.display.flip()
+    '''
+    x, y = 50, 50
+    running = True
+    # 开启一个事件循环处理发生的事件
+    while running:
+        # 从消息队列中获取事件并对事件进行处理
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((255, 255, 255))
+        pygame.draw.circle(screen, (255, 0, 0), (x, y), 30, 0)
+        pygame.display.flip()
+        # 每隔 50ms 就改变小球的位置并刷新窗口
+        pygame.time.delay(50)
+        x, y = x + 5, y + 5
+
+
+if __name__ == '__main__':
+    main()
