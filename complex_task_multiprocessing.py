27942f00d9b1268b6a3e53197b9f7f29cb939b28
Yao-Phoenix <493867456@qq.com>
2020-01-09 15:24:42 +0800

    多进程处理复杂任务

---
+++ complex_task_multiprocessing.py
@@ -0,0 +1,36 @@
+from time import time
+from multiprocessing import Process, Queue
+from random import randint
+
+def task_handler(curr_list, result_queue):
+    total = 0
+    for number in curr_list:
+        total += number
+    result_queue.put(total)
+
+def main():
+    processes = []
+    number_list = [x for x in range(1, 100000001)]
+    result_queue = Queue()
+    index = 0
+    # 启动 8 个进程将数据切片后进行运算
+    for _ in range(8):
+        p = Process(target=task_handler,
+                args=(number_list[index:index + 12500000], result_queue))
+        index += 12500000
+        processes.append(p)
+        p.start()
+
+    # 开始记录所有进程执行完成花费的时间
+    start = time()
+    for p in processes:
+        p.join()
+    total = 0
+    while not result_queue.empty():
+        total += result_queue.get()
+    print(total)
+    end = time()
+    print('Execution time: {}s'.format(end - start))
+
+if __name__ == '__main__':
+    main()
